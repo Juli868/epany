e140ff05ec6a257e334ff2fd5d06e2e99e49ca0f
juli868 <bugijul@gmail.com>
2022-11-18 10:25:03 +0200

    initial commit

---
+++ MEan.js
@@ -0,0 +1,11 @@
+function names(a,b,age)
+{
+    if (age>=20)
+    {
+    console.log(`You're an adult`)
+    }
+    else{
+        console.log(`you're young`)
+    }
+}
+names("Cedrick","Enock",19)
---
+++ trial1.js
@@ -0,0 +1,5 @@
+let num1=Jack, num2=Nsabimana
+//let sum= num1+num2
+//console.log(`the first number is ${num1} ssecond number is ${num2} and their sum is ${sum}`)
+//console.log("the entered number is",num1,"and",num2,"and their sum is ",sum,)
+console.log ("the first name is "+num1+ "and the second number is"+num2+)
